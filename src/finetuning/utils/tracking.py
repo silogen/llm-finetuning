fa7e2121217e7602d648ec9d9a707348556d8275
Aku Rouhe <akurouhe@gmail.com>
2026-04-28 15:48:40 +0300

    Fix name generation

--- src/finetuning/utils/tracking.py
+++ src/finetuning/utils/tracking.py
@@ -1,10 +1,21 @@
 """Components of finetuning pipeline"""
 
 import os
+import random
+import string
 
 import mlflow
 
 from finetuning.config import DPOExperimentConfig, SFTExperimentConfig
+
+
+def generate_random_name():
+    """Generate a random name"""
+    prev_state = random.getstate()
+    random.seed()
+    random_name = "".join(random.choices(string.ascii_letters + string.digits + "-", k=8))
+    random.setstate(prev_state)
+    return random_name
 
 
 def setup_tracking(exp_conf: SFTExperimentConfig | DPOExperimentConfig, args):
@@ -18,17 +29,17 @@ def setup_tracking(exp_conf: SFTExperimentConfig | DPOExperimentConfig, args):
     if args.mlflow_server_uri is not None:
         mlflow.set_tracking_uri(args.mlflow_server_uri)
         mlflow.set_experiment(args.experiment_name)
-        mlflow.set_tag("mlflow.runName", mlflow.utils.name_utils._generate_random_name())
+        mlflow.set_tag("mlflow.runName", generate_random_name())
         os.environ["MLFLOW_FLATTEN_PARAMS"] = "TRUE"
         os.environ["HF_MLFLOW_LOG_ARTIFACTS"] = args.hf_mlflow_log_artifacts
     elif exp_conf.tracking is not None:
         mlflow.set_tracking_uri(exp_conf.tracking.mlflow_server_uri)
         mlflow.set_experiment(exp_conf.tracking.experiment_name)
-        mlflow.set_tag("mlflow.runName", mlflow.utils.name_utils._generate_random_name())
+        mlflow.set_tag("mlflow.runName", generate_random_name())
         os.environ["MLFLOW_FLATTEN_PARAMS"] = "TRUE"
         os.environ["HF_MLFLOW_LOG_ARTIFACTS"] = exp_conf.tracking.hf_mlflow_log_artifacts
     elif "MLFLOW_TRACKING_URI" in os.environ:
-        mlflow.set_tag("mlflow.runName", mlflow.utils.name_utils._generate_random_name())
+        mlflow.set_tag("mlflow.runName", generate_random_name())
     elif "MLFLOW_TRACKING_URI" not in os.environ:
         os.environ["DISABLE_MLFLOW_INTEGRATION"] = "TRUE"
 
